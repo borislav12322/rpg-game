55a90a57da6b32709944c52658a378c256091b0f
Borislav <borislavizmestiev@gmail.com>
2021-06-29 02:12:02 +0300

    webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require("path");
 const HTMlWebpackPlugin = require("html-webpack-plugin");
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
-    mode: "development",
+    mode: NODE_ENV || "development",
     entry: path.resolve(__dirname, "src/index.js"),
     output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
     },
-    watch: true,
+    watch: NODE_ENV === 'development',
     watchOptions: {
-    ignored: /node_modules/,
-    poll: 1000,
+        ignored: /node_modules/,
+        poll: 1000,
     },
     module: {
     rules: [
@@ -44,10 +46,11 @@ module.exports = {
     }),
     ],
     devServer: {
-    port: 3000,
-    overlay: true,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
+        port: 3000,
+        overlay: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
     },
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
